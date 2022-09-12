c836e1460d13ec236ea0525450ae0522380a74b2
Andrey0201 <andriutsa1@ukr.net>
2022-09-12 22:11:05 +0300

    create new branch and fixed

--- src/com/javapro/lesson8/ArrayValueCalculator.java
+++ src/com/javapro/lesson8/ArrayValueCalculator.java
@@ -3,37 +3,38 @@ package com.javapro.lesson8;
 
 public class ArrayValueCalculator {
 
-  public void doCalc(String[][] arr) throws ArraySizeException, ArrayDataException,
-      StringIndexOutOfBoundsException,NullPointerException {
-    int sum = 0;
-    int a = 0;
-    if (arr.length != 4) {
-      throw new ArraySizeException("Wrong number of lines");
-    } else {
-      for (int i = 0; i < arr.length; i++) {
-        if (arr[i].length != 4) {
-          throw new ArraySizeException(String.format("Wrong number of columns in %d line", i));
+    public void doCalc(String[][] arr) throws ArraySizeException, ArrayDataException {
+        checkArraySize(arr);
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                String s = arr[i][j];
+                int a = 0;
+                try {
+                    a = Integer.parseInt(s);
+                } catch (RuntimeException e) {
+                    throw new ArrayDataException(i, j);
+                }
+                sum = sum + a;
+//                System.out.print(a + " ");
+            }
+//            System.out.println();
         }
-        for (int j = 0; j < arr[i].length; j++) {
-          String s = arr[i][j];
-          if (s == null) {
-            throw new NullPointerException("String is null");
-          }  if (s.charAt(0) == 0) {
-            throw new StringIndexOutOfBoundsException();
-          } else if (!Character.isDigit(s.charAt(0))) {
-            throw new ArrayDataException(String.format("'%s'-not a number in cell[%d,%d] ",
-                s, i, j));
-          }
-          a = Integer.parseInt(s);
-          sum = sum + a;
-         // System.out.print(a + " ");
-        }
-     //   System.out.println();
-      }
+        System.out.println("total sum all element " + sum);
     }
-    System.out.println("total sum all element " + sum);
 
-  }
+
+    private void checkArraySize(String[][] arr) throws ArraySizeException {
+        if (arr.length != 4) {
+            throw new ArraySizeException();
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i].length != 4) {
+                    throw new ArraySizeException();
+                }
+            }
+        }
+    }
 }
 
 
--- src/com/javapro/lesson8/Main.java
+++ src/com/javapro/lesson8/Main.java
@@ -2,80 +2,73 @@ package com.javapro.lesson8;
 
 public class Main {
 
-  public static void main(String[] args) {
-    ArrayValueCalculator calculator = new ArrayValueCalculator();
+    public static void main(String[] args) {
+        ArrayValueCalculator calculator = new ArrayValueCalculator();
 
-    String[][] correct = {
-        {"1000", "2", "3", "4"},
-        {"1", "2", "3", "4"},
-        {"1", "2", "3", "4"},
-        {"1", "2", "3", "4"}
+        String[][] correct = {
+                {"1000", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"}
+        };
+        String[][] incorrect1 = {
+                {"1000", "2", "3", "4"},
+                {"1", "2", "", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"}
+        };
+        String[][] incorrect2 = {
+                {"1000", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3",},
+                {"1", "2", "3", "4"}
+        };
+        String[][] incorrect3 = {
+                {"1000", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "Hello word"},
+                {"1", "2", "3", "4"}
+        };
 
-    };
-    String[][] incorrect5 = {
-        {"1000", "2", "3", "4"},
-        {"1", "2", "", "4"},
-        {"1", "2", "3", "4"},
-        {"1", "2", "3", "4"}
-    };
-    String[][] incorrect4 = {
-        {"1000", "2", "3", "4"},
-        {"1", "2", "3", "4"},
-        {"1", "2", "3",},
-        {"1", "2", "3", "4"}
-    };
-    String[][] incorrect3 = {
-        {"1000", "2", "3", "4"},
-        {"1", "2", "3", "4"},
-        {"1", "2", "3", "Hello word"},
-        {"1", "2", "3", "4"}
-    };
+        String[][] incorrect4 = {
+                {"1", "2", "f", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"}
+        };
 
-    String[][] incorrect = {
-        {"1", "2", "f", "4"},
-        {"1", "2", "3", "4"},
-        {"1", "2", "3", "4"}
-    };
+        String[][] incorrect5 = new String[4][4];
 
-    String[][] incorrect2 = new String[4][4];
-
-    try {
-      calculator.doCalc(correct);
-    } catch (ArraySizeException | ArrayDataException | NullPointerException |
-             StringIndexOutOfBoundsException e) {
-      System.out.println(e.getMessage());
+        try {
+            calculator.doCalc(correct);
+        } catch (ArraySizeException | ArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            calculator.doCalc(incorrect1);
+        } catch (ArraySizeException | ArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            calculator.doCalc(incorrect2);
+        } catch (ArraySizeException | ArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            calculator.doCalc(incorrect3);
+        } catch (ArraySizeException | ArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            calculator.doCalc(incorrect4);
+        } catch (ArraySizeException | ArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            calculator.doCalc(incorrect5);
+        } catch (ArraySizeException | ArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
     }
-    try {
-      calculator.doCalc(incorrect2);
-    } catch (ArraySizeException | ArrayDataException | NullPointerException |
-             StringIndexOutOfBoundsException e) {
-      System.out.println("In input array incorrect2: " + e.getMessage());
-    }
-    try {
-      calculator.doCalc(incorrect);
-    } catch (ArraySizeException | ArrayDataException | NullPointerException |
-             StringIndexOutOfBoundsException e) {
-      System.out.println("In input array incorrect: " + e.getMessage());
-    }
-    try {
-      calculator.doCalc(incorrect3);
-    } catch (ArraySizeException | ArrayDataException | NullPointerException |
-             StringIndexOutOfBoundsException e) {
-      System.out.println("In input array incorrect3: " + e.getMessage());
-    }
-    try {
-      calculator.doCalc(incorrect4);
-    } catch (ArraySizeException | ArrayDataException | NullPointerException |
-             StringIndexOutOfBoundsException e) {
-      System.out.println("In input array incorrect3: " + e.getMessage());
-    }
-    try {
-      calculator.doCalc(incorrect5);
-    } catch (ArraySizeException | ArrayDataException | NullPointerException |
-             StringIndexOutOfBoundsException e) {
-      System.out.println("In input array incorrect5: " + e.getMessage());
-    }
-  }
 }
 
 
